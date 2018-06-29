833a963364b26cc3b2c88ad02a49425f54f5962f
GavinMK <gavinkgrn@gmail.com>
2018-06-29 12:03:23 +0100

    Thread details and resolver. Dumb presentational details component.

---
+++ src/app/threads/thread-details/thread-details.component.ts
@@ -0,0 +1,31 @@
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {Thread} from '../../models/thread';
+import {ThreadsService} from '../threads.service';
+import {ActivatedRoute, Router} from '@angular/router';
+import {map} from 'rxjs/operators';
+
+
+@Component({
+  selector: 'app-thread-details',
+  templateUrl: './thread-details.component.html',
+  styleUrls: ['./thread-details.component.css']
+})
+export class ThreadDetailsComponent implements OnInit {
+
+  thread: Thread;
+
+  constructor(
+    private threadService: ThreadsService,
+    private route: ActivatedRoute,
+    private router: Router) {
+
+  }
+
+  ngOnInit() {
+    this.route.data.pipe(
+      map(data => data.thread))
+      .subscribe(thread => { this.thread = thread;
+      console.log(this.thread)});
+  }
+
+}
