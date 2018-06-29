18f21dfe91f7e35fe5fb97ab238856edd024184b
GavinMK <gavinkgrn@gmail.com>
2018-06-29 08:10:23 +0100

    Added Service to test REST api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import {Component, OnInit } from '@angular/core';
+import { User } from './models/user';
+import {UserService} from './users/user.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  user: User;
+  constructor(private userService: UserService) {}
+  ngOnInit() {
+    this.userService.getUser('0').subscribe(user => this.user = user as User);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {UserService} from './users/user.service';
+import {API_PATH} from './app.tokens';
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    UserService,
+    { provide: API_PATH, useValue: 'http://localhost:3000'}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
